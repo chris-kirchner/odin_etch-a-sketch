045f95997dc201e3886ae687ee315655cb1f988a
Chris <106388029+chris-kirchner@users.noreply.github.com>
2023-05-08 13:51:59 -0700

    Change grid size to fit parent container dimensions

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 const gridContainer = document.getElementById("grid-container");
-// gridContainer.appendChild(square);
-let count = 0;
-const lineCount = 16;
+let gridContainerStyle = getComputedStyle(gridContainer);
+let gridWidth = parseInt(gridContainerStyle.width.replace(/\D/g, ""));
+const lineCount = 50;
+const containerSize = gridWidth / lineCount;
+// let count = 0;
+
 for (let i = 0; i < lineCount; i++) {
   for (let j = 0; j < lineCount; j++) {
-    count ++;
+    // count ++;
     const square = document.createElement("div");
     square.className += "square";
+    square.style.height = containerSize + "px";
+    square.style.width = containerSize + "px";
     gridContainer.appendChild(square);
-    square.innerText = count;
-  }
-}
+    // square.innerText = count;
+  };
+};
 
 let squares = document.querySelectorAll(".square");
 
