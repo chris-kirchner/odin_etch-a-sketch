12d0b72a6cdea5c24954ab5bde1004ba1d209e1c
Chris <106388029+chris-kirchner@users.noreply.github.com>
2023-05-10 16:27:40 -0700

    Add color change on repeat mouseleaves

--- script.js
+++ script.js
@@ -36,17 +36,49 @@ function squareSetup(lineCount) {
       square.style.height = containerSize + "px";
       square.style.width = containerSize + "px";
       gridContainer.appendChild(square);
-      square.innerText = count;
+      // square.innerText = count;
       square.style.fontSize = "8px";
       square.style.textAlign = "center";
     }
   }
-  let squares = document.querySelectorAll(".square");
-  squares.forEach((square) => {
-    square.addEventListener("mouseover", () => {
-      square.classList.add("active");
-    })
-  })
+  squareEvent();
 };
 
 squareSetup(20);
+
+function squareEvent() {
+  let squares = document.querySelectorAll(".square");
+  squares.forEach(square => {
+    let bgColor = "";
+    square.addEventListener("mouseenter", () => {
+      bgColor = square.style.backgroundColor;
+      square.classList.add("active");
+      square.style.backgroundColor = "rgb(255, 165, 0)";
+    })
+    square.addEventListener("mouseleave", () => {
+      if (bgColor === "") {
+        square.style.backgroundColor = "rgb(255, 0, 0)";
+      }
+      else {
+        let colorChange = 20;
+        let regex = /\d+/g;
+        let rgbArr = bgColor.match(regex);
+        for (let i = 0; i < rgbArr.length; i++) {
+          parseInt(rgbArr[i]);
+          if ((rgbArr[i] - colorChange) >= 0) {
+            rgbArr[i] -= colorChange;
+          }
+          else {
+            rgbArr[i] = 0;
+          }
+        }
+        square.style.backgroundColor = "rgb("+rgbArr[0]+", "+rgbArr[1]+", "+rgbArr[2]+")";
+      }
+    });
+  });
+};
+
+// let regex = /\d+/g;
+// let str = "rgb(255,0,0)";
+// let x = str.match(regex);
+// console.log(parseInt(x[1]));
