35aaf459977a5fbf64a0a699abd089a6bbbfaf58
Chris <106388029+chris-kirchner@users.noreply.github.com>
2023-06-06 14:09:35 -0700

    Add eventListeners for title container

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const gridContainer = document.getElementById("grid-container");
 const bgContainer = document.getElementById("bg-container");
 let bgSquare = document.getElementsByClassName("bg-square");
+let titleContainer = document.getElementById("title-container");
+let titleBevel = document.getElementsByClassName("title-bevel");
 const button = document.getElementById("button");
 const buttonReset = document.getElementById("button-reset");
 const buttonGridResize = document.getElementById("button-grid-resize");
@@ -16,12 +18,28 @@ let gridContainerStyle = getComputedStyle(gridContainer);
 let gridWidth = parseInt(gridContainerStyle.width.replace(/\D/g, ""));
 let squaresPerLine = 50;
 
-/* Event handlers for changing button color on enter and leave */
+/* Mouse enter/leave listeners for title container */
+titleContainer.addEventListener("mouseenter", bevelShadowOff);
+titleContainer.addEventListener("mouseleave", bevelShadowOn);
+
+/* Adds className to title bevel container changing css style of drop shadow */
+function bevelShadowOff() {
+  titleBevel[0].className = "title-bevel title-hover";
+};
+
+/* Returns className of title bevel container to default css style */
+function bevelShadowOn() {
+  titleBevel[0].className = "title-bevel";
+};
+
+/* Event listeners for changing button color on mouse enter and leave */
 for (let i = 0; i < buttonContainer.length; i++) {
   buttonContainer[i].addEventListener("mouseenter", buttonHighlight);
   buttonContainer[i].addEventListener("mouseleave", buttonFade);
 };
 
+/* Event listener setup for buttonContainer[0] and the input field
+that exists on top of it */
 buttonContainer[0].addEventListener("mouseenter", inputScaleMouseEnter);
 buttonContainer[0].addEventListener("mouseleave", inputScaleMouseLeave);
 buttonContainer[0].addEventListener("mouseup", inputScaleMouseUp);
@@ -29,36 +47,44 @@ buttonContainer[0].addEventListener("mousedown", inputScaleMouseDown);
 inputGridSize.addEventListener("mouseenter", inputMouseEnter);
 inputGridSize.addEventListener("mouseleave", inputMouseLeave);
 
+/* On mouse enter, handles the style to scale the input field
+and position it correctly */
 function inputScaleMouseEnter() {
   inputGridSize.style.transform = "scale(1.02)";
   inputGridSize.style.transition = "transform 0.4s, left 0.3s";
   inputGridSize.style.left = "178.6px";
 };
 
+/* On mouse leave, scales and positions the input field */
 function inputScaleMouseLeave() {
   inputGridSize.style.transform = "scale(1.0)";
   inputGridSize.style.transition = "transform 0.4s ease-out, left 0.3s ease-out";
   inputGridSize.style.left = "177px";
 };
 
+/* On mouse up, scales and positions the input field */
 function inputScaleMouseUp() {
   inputGridSize.style.transform = "scale(1.02)";
   inputGridSize.style.transition = "transform 0.3s, left 0.2s";
   inputGridSize.style.left = "178.3px";
 };
 
+/* On mouse down, scales and positions the input field */
 function inputScaleMouseDown() {
   inputGridSize.style.transform = "scale(1.0)";
   inputGridSize.style.left = "177px";
   inputGridSize.style.transition = "transform 0.15s, left 0.15s";
 };
 
+/* On mouse enter, maintains the correct scaling and position for
+the input field and keeps the button the correct scale */
 function inputMouseEnter() {
   inputGridSize.style.transform = "scale(1.02)";
   inputGridSize.style.left = "178.3px";
   buttonContainer[0].style.transform = "scale(1.02)";
 };
 
+/* On mouse leave, removes transform style from buttonContainer[0] */
 function inputMouseLeave() {
   buttonContainer[0].style.transform = "";
 };
@@ -78,13 +104,16 @@ function buttonFade(e) {
 currentGridSize.innerText = inputGridSize.value + " x " + inputGridSize.value;
 
 /* Changes the string value on the grid size change button 
-to the right of the user input field */
+to the right of the input field */
 inputValue.innerText = " x " + inputGridSize.value;
 
-/* Event listener for the user input field*/
+/* Event listener for any input in the input field*/
 inputGridSize.addEventListener("input", inputGridSizeField);
+
+/* Mouse enter event listener for the input field */
 inputGridSize.addEventListener("mouseenter", buttonHighlight2);
 
+/* Adds class names specifically to buttonContainer[0] giving it a hover style */
 function buttonHighlight2() {
   buttonContainer[0].className = "button-container btn-hover btn-bevel-highlight";
 };
@@ -108,7 +137,8 @@ function inputGridSizeField(e) {
 buttonGridResize.addEventListener("click", changeGridSize);
 
 /* Sets up a new grid based on the value in the user input field
-as long as it is not the same value as previously entered. */
+as long as it is not the same value as previously entered.
+Will also call the function to change the current grid size text value. */
 function changeGridSize() {
   let value = parseInt(inputGridSize.value);
   if (value !== squaresPerLine && value >= 20 && value <= 80) {
@@ -129,16 +159,20 @@ function changeGridSize() {
   currentGridSize.innerText = inputGridSize.value + " x " + inputGridSize.value;
 };
 
+/* Adds text-highlight class to currentGridSize then delays the call
+to remove the class, giving the effect of text changing color briefly,
+then returning to the original color */
 function currentGridSizeHighlight() {
   currentGridSize.className = "text-highlight";
   setTimeout(currentGridSizeFade, 500);
 }
 
+/* Removes text-highlight class */
 function currentGridSizeFade() {
   currentGridSize.className = "";
 }
 
-// Click event listener for the "erase and start over" button
+/* Click event listener for the "erase and start over" button */
 buttonReset.addEventListener("click", gridReset);
 
 /* Removes all child nodes of gridContainer then sets up the same
@@ -148,7 +182,7 @@ function gridReset() {
   squareSetup(squaresPerLine);
 };
 
-// Removes all children of a parent container
+/* Removes all children of a parent container */
 function removeAllChildNodes(parent) {
   while (parent.firstChild) {
     parent.removeChild(parent.firstChild);
@@ -207,7 +241,7 @@ function squareEvent() {
 };
 
 /* Setup function for the background: creates squares and randomizes
-opacity values for each square, then calls squareSetup() function */
+opacity values for each square, then calls the grid setup (squareSetup) function */
 function bgSquareSetup(num) {
   squaresPerLine = parseInt(num);
   const containerSize = window.innerWidth / num;
@@ -226,4 +260,4 @@ function bgSquareSetup(num) {
   squareSetup(50);
 };
 
-bgSquareSetup(46);
+bgSquareSetup(2);
